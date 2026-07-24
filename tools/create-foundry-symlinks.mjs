9a5fe386e8210dc3b14931057b044e213ce8f4d3
Dave <dave.belodri@gmail.com>
2026-07-24 22:04:36 +0200

    Refactor and simplify create-foundry-symlinks tool

--- tools/create-foundry-symlinks.mjs
+++ tools/create-foundry-symlinks.mjs
@@ -1,116 +1,70 @@
 /* eslint-disable jsdoc/require-jsdoc */
 import * as fs from "fs";
-import yaml from "js-yaml";
-import path from "path";
+import path, { dirname } from "path";
+import { parseArgs } from "util";
 
-/*
-    Env vars:
-    "foundry-config-path": string (default "foundry-config.yaml")
-    "dest-root-dir": string (default "foundry")
-*/
-await execute()
+const DEST_ROOT_DIR = "foundry";  // Hardcoded because it's referenced in jsconfig
+const ABORT = "abort";
 
-async function execute() {
+await (async () => {
     process.exitCode = 1;
 
-    const options = getEnvOptions();
+    const options = getOptions();
+    if(!options) return;
 
-    const config = await loadConfig(options.foundryConfigPath);
-    if(!config) return;
+    const sourceRootDir = getSourceRootDir(options.installPath);
+    if(!sourceRootDir) return;
 
-    const sourceRootDir = getSourceRootDir(config);
-    if(!sourceRootDir) return false;
+    // Create destinatin dir if it doesn't already exist.
+    await fs.promises.mkdir(DEST_ROOT_DIR, { recursive: true });
 
-    await createDestinationRootDir(options.destRootDir);
-    await linkJsFiles(sourceRootDir, options.destRootDir);
-    await linkLangFiles(sourceRootDir, options.destRootDir);
+    // Link JS files
+    for(const p of ["client", "common", "tsconfig.json"]) {
+        const targetPath = path.join(sourceRootDir, p);
+        const symlinkPath = path.join(DEST_ROOT_DIR, p);
+
+        if(await tryCreateSymlink(targetPath, symlinkPath) === ABORT) return;
+    }
+
+    // Link Lang files
+    const targetPath = path.join(sourceRootDir, "public", "lang");
+    const symlinkPath = path.join(DEST_ROOT_DIR, "lang");
+
+    if(await tryCreateSymlink(targetPath, symlinkPath) === ABORT) return;
 
     console.log("Successfully created foundry symlinks.");
     process.exitCode = 0;
-}
+})();
 
-/**
- * @typedef {object} EnvOptions
- * @property {string} foundryConfigPath
- * @property {string} destRootDir
- */
 
-function getEnvOptions() {
-    const foundryConfigPath = process.env.npm_config_foundry_config_path ?? "foundry-config.yaml";
-    const destRootDir = process.env.npm_config_dest_root_dir ?? "foundry";
+function getOptions() {
+    const { values: parsedArgs } = parseArgs({ 
+        options: {
+            'foundry-install-path': { type: "string" }
+        }
+    });
 
-    /** @type {EnvOptions} */
-    const options = {
-        foundryConfigPath,
-        destRootDir
+    const opts = {
+        installPath: parsedArgs["foundry-install-path"]
     }
 
-    return options
-}
+    if(!opts.installPath) return console.error("Missing required argument '--foundry-install-path=<string>'");
+    if(!fs.existsSync(opts.installPath)) return console.error(`Foundry install path does not exist: ${opts.installPath}`);
 
-/**
- * @param {string} sourceRootDir 
- * @param {string} destRootDir
- */
-async function linkJsFiles(sourceRootDir, destRootDir) {
-    for(const p of ["client", "common", "tsconfig.json"]) {
-        const targetPath = path.join(sourceRootDir, p);
-        const symlinkPath = path.join(destRootDir, p);
-
-        const canCreate = await canCreateSymlink(targetPath, symlinkPath)
-        if(canCreate) await createSymlink(targetPath, symlinkPath);
-    }
-}
-
-async function linkLangFiles(sourceRootDir, destRootDir) {
-    const targetPath = path.join(sourceRootDir, "public", "lang");
-    const symlinkPath = path.join(destRootDir, "lang");
-
-    const canCreate = await canCreateSymlink(targetPath, symlinkPath)
-    if(canCreate) await createSymlink(targetPath, symlinkPath);
-}
-
-async function createSymlink(targetPath, symlinkPath) {
-    await fs.promises.symlink(targetPath, symlinkPath);
-    console.log(`Symlinked: ${symlinkPath} -> ${targetPath}`);
+    return opts;
 }
 
 
 /**
- * @typedef {object} Config
- * @property {string} installPath
+ * @param {string} foundryInstallPath 
  */
-
-/**
- * @param {string} configYamlPath 
- */
-async function loadConfig(configYamlPath) {
-    if(!fs.existsSync(configYamlPath)) {
-        console.error(`Config yaml file '${configYamlPath}' not found.`);
-        return;
-    }
-
-    try {
-        const fc = await fs.promises.readFile(configYamlPath, "utf-8");
-
-        /** @type {Config} */
-        const foundryConfig = yaml.load(fc);
-        return foundryConfig
-    } catch (err) {
-        console.error(`Error reading ${configYamlPath}: ${err}`);
-    }
-}
-
-/**
- * @param {Config} config 
- */
-function getSourceRootDir(config) {
+function getSourceRootDir(foundryInstallPath) {
     // As of 13.338, the Node install is not nested but electron installs are
-    const isNested = fs.existsSync(path.join(config.installPath, "resources", "app"));
+    const isNested = fs.existsSync(path.join(foundryInstallPath, "resources", "app"));
 
     const root = isNested 
-        ? path.join(config.installPath, "resources", "app")
-        : config.installPath;
+        ? path.join(foundryInstallPath, "resources", "app")
+        : foundryInstallPath;
 
     if(!fs.existsSync(root)) {
         console.error(`Foundry install path '${root}' does not exist.`);
@@ -121,38 +75,31 @@ function getSourceRootDir(config) {
 }
 
 /**
- * @param {string} dirName 
- */
-async function createDestinationRootDir(dirName) {
-    await fs.promises.mkdir(dirName, { recursive: true });
-}
-
-/**
  * @param {string} targetPath 
- * @param {string} symlinkPath 
+ * @param {string} symlinkPath
  */
-async function canCreateSymlink(targetPath, symlinkPath) {
+async function tryCreateSymlink(targetPath, symlinkPath) {
     const stat = await fs.promises.lstat(symlinkPath, { throwIfNoEntry: false });
 
-    // Is the path empty?
-    if(!stat) return true;
+    // Does the path already exist?
+    if(stat) {
+        // Is it a non-symlink file/dir?
+        if(!stat.isSymbolicLink()) {
+            console.error(`Unable to create symlink because file/directory exists at: ${symlinkPath}`);
+            console.error("Remove existing file/directory and run this script again.");
+            return ABORT;
+        }
 
-    // Is it a non-symlink file/dir?
-    if(!stat.isSymbolicLink()) {
-        console.error(`Unable to create symlink because file/directory exists at: ${symlinkPath}`);
-        console.error("Remove existing file/directory and run this script again.");
-        return false;
+        // Is the existing symlink already correct?
+        const currentTarget = await fs.promises.readlink(symlinkPath);
+        const absoluteCurrent = path.resolve(path.dirname(symlinkPath), currentTarget); // Resolve paths to handle both absolute and relative targets accurately
+        const absoluteTarget = path.resolve(targetPath);
+        if(absoluteCurrent === absoluteTarget) return;
+
+        console.log(`Unlinking existing symlink: ${symlinkPath}`);
+        await fs.promises.unlink(symlinkPath);
     }
 
-    // Is existing symlink correct?
-    const currentTarget = await fs.promises.readlink(symlinkPath);
-    const absoluteCurrent = path.resolve(path.dirname(symlinkPath), currentTarget); // Resolve paths to handle both absolute and relative targets accurately
-    const absoluteTarget = path.resolve(targetPath);
-    if(absoluteCurrent === absoluteTarget) return false;
-
-
-    console.log(`Unlinking existing symlink: ${symlinkPath}`);
-    await fs.promises.unlink(symlinkPath);
-
-    return true;
+    await fs.promises.symlink(targetPath, symlinkPath);
+    console.log(`Symlinked: ${symlinkPath} -> ${targetPath}`);
 }
